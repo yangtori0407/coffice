b0c9c4cf9e9ac84f69623dbe005eff46f0f78dec
ddiske <ddiske@naver.com>
2025-05-29 15:55:27 +0900

    VO_ddiske

---
+++ src/main/java/com/coffice/app/term/schedule/ScheduleVO.java
@@ -0,0 +1,14 @@
+package com.coffice.app.term.schedule;
+
+import com.coffice.app.term.TermVO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScheduleVO extends TermVO {
+	
+	private String detail;
+
+}
