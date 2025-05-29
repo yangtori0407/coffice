2020fdb8a518edbcf7aa103e3e740232696e12e9
yangtori0407 <heyang236@gmail.com>
2025-05-29 11:45:21 +0900

    init

---
+++ src/main/java/com/coffice/app/CofficeApplication.java
@@ -0,0 +1,13 @@
+package com.coffice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffice/app/CofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffice.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
