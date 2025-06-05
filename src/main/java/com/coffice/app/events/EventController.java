a059eac060fe48a0e7c722805bea9de60853f842
ddiske <ddiske@naver.com>
2025-06-05 15:47:30 +0900

    chore: #21 controller, js, jsp 간소화

---
+++ src/main/java/com/coffice/app/events/EventController.java
@@ -0,0 +1,24 @@
+package com.coffice.app.events;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/events/*")
+public class EventController {
+	
+	@GetMapping("schedule")
+	public String getSchedule(Model model) {
+		model.addAttribute("kind", "일정 관리");
+		return "events/schedule";
+	}
+	
+	@GetMapping("vacation")
+	public String getVacation(Model model) {
+		model.addAttribute("kind", "휴가 관리");
+		return "events/schedule";
+	}
+
+}
---
+++ src/main/resources/static/js/calendar/fullCalendar.js
@@ -0,0 +1,84 @@
+const kind = document.getElementById("kind")
+
+var calendarEl = document.getElementById("calendar")
+var calendar = new FullCalendar.Calendar(calendarEl, {
+themeSystem: 'bootstrap4',
+locale: 'ko',
+customButtons: {
+    addButton: {
+    text: '+',
+    click: function() {
+        console.log('add')
+    }
+    }
+},
+nowIndicator: true,
+headerToolbar: {
+    left:'prevYear,prev,next,nextYear today editButton',
+    center: 'title',
+    right: 'addButton dayGridMonth,listWeek'
+},
+    initialDate: Date.now(),
+    navLinks: false, // can click day/week names to navigate views
+    editable: false,
+    dayMaxEvents: true, // allow "more" link when too many events
+    fixedWeekCount: false,
+    dayCellContent: function (arg) {
+    const { date } = arg;
+    return date.getDate();
+    },
+    dateClick: function(e) {
+    console.log(e)
+    },
+    eventClick: function(e) {
+        console.log(e.event)
+    },
+    eventDrop: function(e) {
+    console.log("EventDrop", e.event.start)
+        let t = new Date(e.event.start)
+    console.log(t.getHours())
+    },
+    eventResize: function(e) {
+    console.log("EventResize", e.event.start)
+    let t = new Date(e.event.start)
+    console.log(t.getHours())
+    }
+});
+
+for(let i = 0; i < 3; i++) {
+    let list = JSON.parse(localStorage.getItem("list"+i))
+
+        for(a of list) {
+
+            let event = {
+                title: a.dateName,
+                start: a.locdate.toString(),
+                allDay: true,
+                color: '#378006'
+            }
+            calendar.addEvent(event);
+        }
+}
+
+if(kind.innerText.trim() == '일정 관리') {
+    let event = {
+                title: 'test',
+                start: '20250605',
+                allDay: true,
+                color: '#378006'
+            }
+            calendar.addEvent(event);
+            calendar.render()
+}
+
+if(kind.innerText.trim() == '휴가 관리') {
+    let event = {
+                title: 'test',
+                start: '20250605',
+                allDay: true,
+                color: '#000000'
+            }
+            calendar.addEvent(event);
+}
+
+calendar.render();
