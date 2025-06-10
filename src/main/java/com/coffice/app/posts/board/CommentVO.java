f648bb1a5b24839786ae8f850c90fda6df4f9aac
yangtori0407 <114906941+yangtori0407@users.noreply.github.com>
2025-06-10 17:05:03 +0900

    Feat/#30 익명게시판 댓글 (#35)

--- src/main/java/com/coffice/app/posts/board/CommentVO.java
+++ src/main/java/com/coffice/app/posts/board/CommentVO.java
@@ -1,5 +1,9 @@
 package com.coffice.app.posts.board;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,8 +16,21 @@ public class CommentVO {
 	private Long commentNum;
 	private Long boardNum;
 	private String userId;
+	private Timestamp commentDate;
 	private String commentContents;
-	private Long commentRef;
-	private Long commentStep;
-	private Long commentDepth;
+	private Long commentP;
+	private Integer deleteStatus;
+	
+	private Integer isReply;
+	
+	
+	public String getFormatted() {
+		if(getCommentDate() != null) {
+			
+			LocalDateTime localDateTime = this.getCommentDate().toLocalDateTime();
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yy-MM-dd HH:mm");
+			return localDateTime.format(formatter);
+		}
+		return "";
+	}
 }
