2dea9927cd5eba5739c58d79a6f0dda93e013add
yangtori0407 <114906941+yangtori0407@users.noreply.github.com>
2025-06-16 18:59:45 +0900

    Feat/#82 공지사항익명게시판메세지 사이드 바 수정 (#84)

--- src/main/java/com/coffice/app/posts/board/BoardController.java
+++ src/main/java/com/coffice/app/posts/board/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,6 +24,11 @@ public class BoardController {
 	
 	@Autowired
 	private BoardSerivce boardService;
+	
+	@ModelAttribute("posts")
+	public String posts() {
+		return "board";
+	}
 	
 	@GetMapping("list")
 	public String getList(Model model, Pager pager) throws Exception{
