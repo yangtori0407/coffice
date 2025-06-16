a5b409e09d8ee6b1ddc6ddf98c87f71139416782
yangtori0407 <114906941+yangtori0407@users.noreply.github.com>
2025-06-16 19:03:02 +0900

    Revert "Feat/#82 공지사항익명게시판메세지 사이드 바 수정" (#85)

--- src/main/java/com/coffice/app/posts/notice/NoticeController.java
+++ src/main/java/com/coffice/app/posts/notice/NoticeController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,11 +23,6 @@ public class NoticeController {
 	
 	@Autowired
 	private NoticeService noticeService;
-	
-	@ModelAttribute("posts")
-	public String kind() {
-		return "notice";
-	}
 	
 	@GetMapping("list")
 	public String getList(Model model, Pager pager) throws Exception{
--- src/main/resources/static/js/sidebar.js
+++
@@ -1,8 +0,0 @@
-const message = document.getElementById("message");
-const sidebar = document.getElementById("accordionSidebar");
-
-sidebar.addEventListener("click", (e) => {
-    console.log("click");
-    //e.target.classList.add("chat-active");
-    message.classList.remove("chat-active");
-})
