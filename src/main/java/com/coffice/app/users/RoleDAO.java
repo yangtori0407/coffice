726a42c62a20228b6ae140720fd67b5ab5b1969c
erica-co <qkdxks0525@naver.com>
2025-07-01 12:14:08 +0900

    feat: #240 관리자 페이지 필터

--- src/main/java/com/coffice/app/users/RoleDAO.java
+++ src/main/java/com/coffice/app/users/RoleDAO.java
@@ -1,10 +1,13 @@
 package com.coffice.app.users;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface RoleDAO {
 	
 	Long findRoleIdByName (String roleName) throws Exception;
+	
 
 }
