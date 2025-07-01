75203d64328b0a11d1fef2c5a1a8edf03754ebaa
ddiske <ddiske@naver.com>
2025-07-01 19:22:14 +0900

    fix: #253 테스트수정

--- src/main/resources/static/js/user/history.js
+++ src/main/resources/static/js/user/history.js
@@ -1,3 +1,10 @@
+function getCookie(name) {
+    const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return decodeURIComponent(match[2]);
+    return null;
+}
+const uid = getCookie("userId");
+
 document.addEventListener("DOMContentLoaded", function () {
     // tbody 내의 모든 행 선택
     const rows = document.querySelectorAll("table tbody tr");
@@ -13,12 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
         .then(r=>r.json())
         .then(r=>{
             console.log(r)
-            function getCookie(name) {
-                const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-                if (match) return decodeURIComponent(match[2]);
-                return null;
-            }
-            const uid = getCookie("userId");
             let st;
             let et;
             if(r.vacationVO.status == '대기') {
